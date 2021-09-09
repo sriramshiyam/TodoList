1804e31d0d5e0016931d64ed6c99ec04518e991d
Sriram <73679247+sriramshiyam@users.noreply.github.com>
2021-09-08 23:18:37 -1200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+const inputBox = document.querySelector(".inputField input");
+const addBtn = document.querySelector(".inputField button");
+const todoList = document.querySelector(".todoList");
+const clearAll = document.querySelector(".footer button");
+const pendingTask = document.querySelector(".footer span");
+
+inputBox.onkeydown = () => {
+    let userData = inputBox.value;
+    if (userData.trim() != 0) {
+        addBtn.classList.add("active");
+    } else {
+        addBtn.classList.remove("active");
+    }
+};
+
+let liList = [];
+let x = 0;
+let tasks = 0
+
+addBtn.onclick = () => {
+    liList.push(`<li>${inputBox.value}<span onclick="deleteTask(${x})"><i class="fas fa-trash"></i></span></li>`);
+    todoList.innerHTML = liList.join('');
+    inputBox.value = "";
+    x++;
+    tasks++;
+    pendingTask.innerHTML = `You have ${tasks} pending task`;
+};
+
+
+function deleteTask(inde) {
+    let l = liList[inde].split(">")[1].split("<")[0];
+    liList[inde] = `<li class="linethr" style="background: rgb(230, 230, 230);">${l}<span style="background: rgb(27, 141, 255);" onclick="reviveTask(${inde})"><i class="fas fa-redo"></i></span></li>`;
+    todoList.innerHTML = liList.join('');
+    tasks--;
+    pendingTask.innerHTML = `You have ${tasks} pending task`;
+}
+
+function reviveTask(inde) {
+    let l = liList[inde].split(">")[1].split("<")[0];
+    liList[inde] = `<li>${l}<span onclick="deleteTask(${inde})"><i class="fas fa-trash"></i></span></li>`;
+    todoList.innerHTML = liList.join('');
+    tasks++;
+    pendingTask.innerHTML = `You have ${tasks} pending task`;
+}
+
+clearAll.onclick = () => {
+    liList = [];
+    todoList.innerHTML = "";
+    tasks = 0;
+    pendingTask.innerHTML = `You have ${tasks} pending task`;
+};
