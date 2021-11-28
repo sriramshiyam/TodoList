55a48dd4d69796028c8fa60aa6c5ac4be8fd0d32
Sriram <73679247+sriramshiyam@users.noreply.github.com>
2021-11-28 11:42:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,26 +5,31 @@ const clearAll = document.querySelector(".footer button");
 const pendingTask = document.querySelector(".footer span");
 const emptyvalmessage = document.getElementById("emptyinputbox");
 
-addBtn.onclick = () => {
-    let userData = inputBox.value;
-    if (userData.trim().length === 0) {
-        emptyvalmessage.style.display = "block";
-    } else {
-        emptyvalmessage.style.display = "none";
-    }
-};
+addBtn.addEventListener("click", () => {
+  let userData = inputBox.value;
+  if (userData.length === 0) {
+    emptyvalmessage.style.display = "block";
+  } else {
+    emptyvalmessage.style.display = "none";
+  }
+});
 
 let liList = [];
 let x = 0;
 let tasks = 0
 
 addBtn.onclick = () => {
-    liList.push(`<li>${inputBox.value}<span onclick="deleteTask(${x})"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1.25rem" width="1.25rem" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"></path></svg></span></li>`);
-    todoList.innerHTML = liList.join('');
+  let userData = inputBox.value;
+  if (userData.length !== 0) {
+    liList.push(
+      `<li>${inputBox.value}<span onclick="deleteTask(${x})"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16" height="1.25rem" width="1.25rem" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 00-1 1v1a1 1 0 001 1H3v9a2 2 0 002 2h6a2 2 0 002-2V4h.5a1 1 0 001-1V2a1 1 0 00-1-1H10a1 1 0 00-1-1H7a1 1 0 00-1 1H2.5zm3 4a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7a.5.5 0 01.5-.5zM8 5a.5.5 0 01.5.5v7a.5.5 0 01-1 0v-7A.5.5 0 018 5zm3 .5a.5.5 0 00-1 0v7a.5.5 0 001 0v-7z" clip-rule="evenodd"></path></svg></span></li>`
+    );
+    todoList.innerHTML = liList.join("");
     inputBox.value = "";
     x++;
     tasks++;
     pendingTask.innerHTML = `You have ${tasks} pending task`;
+  }
 };
 
 
