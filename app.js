84c0c9b1f3f9d6499e4d37dd2e0d1ead1077a5a1
Sriram <73679247+sriramshiyam@users.noreply.github.com>
2021-12-10 17:29:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ for (let i = 0; i < r; i++) {
         )}<span style="background: rgb(27, 141, 255);" onclick="reviveTask(${i})"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1.25rem" width="1.25rem" xmlns="http://www.w3.org/2000/svg"><path d="M500.33 0h-47.41a12 12 0 0 0-12 12.57l4 82.76A247.42 247.42 0 0 0 256 8C119.34 8 7.9 119.53 8 256.19 8.1 393.07 119.1 504 256 504a247.1 247.1 0 0 0 166.18-63.91 12 12 0 0 0 .48-17.43l-34-34a12 12 0 0 0-16.38-.55A176 176 0 1 1 402.1 157.8l-101.53-4.87a12 12 0 0 0-12.57 12v47.41a12 12 0 0 0 12 12h200.33a12 12 0 0 0 12-12V12a12 12 0 0 0-12-12z"></path></svg></span></li>`
     );
   } else {
+    tasks++;
     liList.push(
       `<li>${localStorage.getItem(
         i
@@ -37,7 +38,6 @@ for (let i = 0; i < r; i++) {
   }
   todoList.innerHTML = liList.join("");
   x++;
-  tasks++;
   pendingTask.innerHTML = `You have ${tasks} pending task`;
 }
 
